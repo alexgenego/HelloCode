7312ad32f1a68042ef7f8eab5839db6c38377fd6
Alex <amkulikov@gmail.com>
2023-02-25 19:16:34 +0300

    выбор и выарианты

---
+++ Example005_Bool_choice/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter Username");
+string username = Console.ReadLine();
+
+if (username.ToLower()== "маша")
+{
+    Console.WriteLine("Ура, это Маша!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
